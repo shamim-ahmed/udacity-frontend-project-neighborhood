b818fe9d9bbbf9a816b993310138f913aa6f9020
Shamim Ahmed <shamim.buet.99@gmail.com>
2016-10-17 22:48:32 +1100

    added a number of markers (corresponding to interesting places) on Google map

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+var Location = function(name, latlng) {
+  this.name = name;
+  this.latlng = latlng;
+};
+
+var appConfig = {
+  center: new Location('Sydney Opera House', new google.maps.LatLng(-33.856783, 151.215290)),
+  locations: [
+    new Location('Government House', new google.maps.LatLng(-33.859621,151.214850)),
+    new Location('Pancakes on The Rocks', new google.maps.LatLng(-33.857165,151.208761)),
+    new Location('Mrs Macquarie\'s Chair', new google.maps.LatLng(-33.860057,151.212271)),
+    new Location('The Spice Room', new google.maps.LatLng(-33.861767,151.212383)),
+    new Location('Frankie\'s Pizza', new google.maps.LatLng(-33.865958,151.209511))
+  ]
+};
 
 $(document).ready(function(){
   $('#sidebar-control').click(function() {
@@ -13,23 +28,31 @@ $(document).ready(function(){
   google.maps.event.addDomListener(window, 'load', initialize);
 
   function initialize() {
-    var latlng = new google.maps.LatLng(40.71, -73.32);
-
     var mapOptions = {
-      center: latlng,
-      scrollWheel: false,
+      center: appConfig.center.latlng,
       mapTypeId: google.maps.MapTypeId.ROADMAP,
-      zoom: 13,
+      zoom: 16,
       disableDefaultUI: true
     };
 
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
     var marker = new google.maps.Marker({
-      position: latlng,
-      url: '/',
+      position: appConfig.center.latlng,
       animation: google.maps.Animation.DROP
     });
 
-    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
     marker.setMap(map);
+
+    var i;
+    var loc;
+    for (i = 0; i < appConfig.locations.length; i++) {
+      loc = appConfig.locations[i];
+      marker = new google.maps.Marker({
+        position: loc.latlng,
+        animation: google.maps.Animation.DROP
+      });
+      marker.setMap(map);
+    }
   }
 });
