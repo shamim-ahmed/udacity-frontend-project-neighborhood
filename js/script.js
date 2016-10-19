66702ba0dc6f22a8c72a7004d450178a9d83f620
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2016-10-19 21:15:36 +1100

    added separate functions to create and display markers

--- js/script.js
+++ js/script.js
@@ -4,19 +4,10 @@ var Location = function(name, latlng) {
   this.isIncluded = ko.observable(true);
 };
 
-var appConfig = {
-  locations: [
-    new Location('Sydney Opera House', new google.maps.LatLng(-33.856783, 151.215290)),
-    new Location('Government House', new google.maps.LatLng(-33.859621, 151.214850)),
-    new Location('Pancakes on The Rocks', new google.maps.LatLng(-33.857165, 151.208761)),
-    new Location('Mrs Macquarie\'s Chair', new google.maps.LatLng(-33.860096, 151.222564)),
-    new Location('The Spice Room', new google.maps.LatLng(-33.861767, 151.212383)),
-    new Location('Frankie\'s Pizza', new google.maps.LatLng(-33.865958, 151.209511))
-  ],
-  center: new google.maps.LatLng(-33.856783, 151.215290)
-};
-
 var map = null;
+var markerArray = [];
+var locations;
+var center;
 
 $(document).ready(function(){
   $('#sidebar-control').click(function() {
@@ -29,27 +20,59 @@ $(document).ready(function(){
     }
   });
 
+  locations = [
+    new Location('Sydney Opera House', new google.maps.LatLng(-33.856783, 151.215290)),
+    new Location('Government House', new google.maps.LatLng(-33.859621, 151.214850)),
+    new Location('Pancakes on The Rocks', new google.maps.LatLng(-33.857165, 151.208761)),
+    new Location('Mrs Macquarie\'s Chair', new google.maps.LatLng(-33.860096, 151.222564)),
+    new Location('The Spice Room', new google.maps.LatLng(-33.861767, 151.212383)),
+    new Location('Frankie\'s Pizza', new google.maps.LatLng(-33.865958, 151.209511))
+  ];
+
+  center = locations[0].latlng;
   google.maps.event.addDomListener(window, 'load', initializeMap);
+
+  // at this point, we have map and markers initialized.
 });
 
 function initializeMap() {
   var mapOptions = {
-    center: appConfig.center,
+    center: center,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     zoom: 16,
     disableDefaultUI: true
   };
 
   map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+  createMarkers();
+  renderMarkers();
+}
 
+function createMarkers() {
   var i, loc, marker;
 
-  for (i = 0; i < appConfig.locations.length; i++) {
-    loc = appConfig.locations[i];
+  for (i = 0; i < locations.length; i++) {
+    loc = locations[i];
     marker = new google.maps.Marker({
       position: loc.latlng,
       animation: google.maps.Animation.DROP
     });
-    marker.setMap(map);
+
+    markerArray.push(marker);
+  }
+}
+
+function renderMarkers() {
+  var i, loc, marker;
+
+  for (i = 0; i < locations.length; i++) {
+    loc = locations[i];
+    marker = markerArray[i];
+
+    if (loc.isIncluded()) {
+      marker.setMap(map);
+    } else {
+      marker.setMap(null);
+    }
   }
 }
